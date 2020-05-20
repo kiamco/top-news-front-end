1001eb8eae70cf4e7c05e7e66b7d3e1d82e58602
kiamco <kiamcoo@gmail.com>
2020-05-20 00:21:48 -0700

    asdasd

---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    tool:{
+        display:"flex",
+        justifyContent:"space-between"
+    }
+});
+
+
+const Navbar = () => {
+    const classes = useStyles();
+    return(
+        <AppBar position='static' color='primary'>
+            <Toolbar className={classes.tool}>
+                <Typography variant='h6'>
+                    Newnews
+                </Typography>
+                <Typography variant='h6'>
+                    About
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+    
+
+}
+
+export default Navbar;
---
+++ src/page/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from '../components/navbar';
+
+const Home = () => {
+    return(
+    <Nav />
+    )
+}
+
+export default Home;
