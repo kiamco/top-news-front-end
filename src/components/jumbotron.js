8bd07c1af8d31fe3f4104caba6c9b70596df587d
kiamco <kiamcoo@gmail.com>
2020-05-20 10:49:53 -0700

    subscribe form

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,1 +1,66 @@
-// write code
+import React,{useState} from 'react';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { FormControl,Input, InputLabel, FormHelperText, Button } from '@material-ui/core';
+
+
+const useStyles = makeStyles(theme => ({
+    container:{
+        height:"425px",
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    header:{
+        textAlign:"center"
+    },
+    form:{
+        minWidth: "650px",
+        display:'flex',
+        flexFlow:"row wrap",
+        alignItems:"center",
+        justifyContent:"space-between",
+                margin: "20px 0 0 0"
+
+    },
+    inputContainer:{
+        minWidth: "500px",
+    },
+    inputEl:{
+        minWidth:"inherit"
+    },
+    sumbmitButton:{
+        margin:"0 0 0 20px"
+    }
+}));
+
+
+const Jumbotron = () => {
+    const classes = useStyles();
+    return(
+        <Box className={classes.container}>
+            <Typography variant='h3' className={classes.header  }>Subscribe to get the latest news</Typography>
+            <FormControl size="medium" required='true' className={classes.form}>
+                <Box className={classes.inputContainer}>
+                    <InputLabel className={classes.inputEl} htmlFor="my-input">Email address</InputLabel>
+                    <Input className={classes.inputEl} id="my-input" aria-describedby="my-helper-text" />
+                    <FormHelperText className={classes.inputEl} id="my-helper-text">We'll never share your email.</FormHelperText>
+                </Box>
+                <Button 
+                size="large" 
+                variant="contained" 
+                className={classes.sumbmitButton}
+                color="primary"
+                type="submit">
+                    Subscribe
+                    </Button>
+            </FormControl>
+
+        </Box>
+    )
+    
+}
+
+export default Jumbotron;
--- src/page/home.js
+++ src/page/home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Nav from '../components/navbar';
-
+import Jumbotron from '../components/jumbotron';
 const Home = () => {
     return(
+    <>
     <Nav />
+    <Jumbotron />
+    </>
     )
 }
 
